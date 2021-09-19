035b3a17b5c24e98cefae13b1f39e725455fd874
neha79091 <neha79091@gmail.com>
2021-09-19 17:37:35 +0530

    containsDuplicate

--- src/LeetcodeQuestions/containsDuplicate.java
+++ src/LeetcodeQuestions/containsDuplicate.java
@@ -1,4 +1,20 @@
+//I used hashmap to keep track of all the integers, whenever one is repeating, containsKey tells me so
+//hence I return true then
+
 package LeetcodeQuestions;
 
-public class containsDuplicate {
+class Solution {
+    public boolean containsDuplicate(int[] nums)
+    {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                return true;
+            }
+            map.put(nums[i],1);
+        }
+        return false;
+    }
 }
